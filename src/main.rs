a56ed247247613f44de184c9296c387c681b0457
BrownCarter <lilbrown3000@gmail.com>
2022-06-25 11:33:30 +0100

    Solution to remove duplicate - Sorted

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
+    let mut v1 = vec![0,0,1,1,1,2,2,3,3,4];
+    let v2 = remove_duplicates(&mut v1);
+    println!("{}", v2);
 }
 
+fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
+    let mut slow = 0;
+    for fast in 1..nums.len() {
+        if nums[fast] != nums[slow] {
+            slow += 1;
+            nums[slow] = nums[fast];
+        }
+    }
+    return slow as i32 + 1;
+}
