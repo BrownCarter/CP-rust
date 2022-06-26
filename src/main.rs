e9ada3584f3e255daac5f48eff1392f05deab227
BrownCarter <lilbrown3000@gmail.com>
2022-06-26 12:48:29 +0100

    Solution to max sub array

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::cmp::max;
+
 fn main() {
+    let v1: Vec<i32> = vec![5, 4, -1, 7, 8];
+    let v2 = max_sub_array(v1);
+    println!("{}", v2);
 }
 
+fn max_sub_array(nums: Vec<i32>) -> i32 {
+    let mut max_sum: i32 = 0;
+    let mut best: i32 = i32::MIN;
+    for s in &nums {
+        max_sum = max(*s, max_sum + *s);
+        best = max(max_sum, best);
+    }
+    return best;
+}
