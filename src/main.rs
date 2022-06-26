c72d5e90ab4052aea7b4afed1e92f1cd17cdb427
BrownCarter <lilbrown3000@gmail.com>
2022-06-26 11:52:15 +0100

    Solution to needle in a haystack

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
+    let i1 = str_str(String::from("hello"), String::from("ll"));
+    println!("{}", i1);
 }
 
+fn str_str(haystack: String, needle: String) -> i32 {
+    if haystack.len() == 0 {
+        return 0;
+    }
+    let check = haystack.find(&needle);
+    match check {
+       Some(check) => return check as i32,
+       None => return -1,
+    }
+}
