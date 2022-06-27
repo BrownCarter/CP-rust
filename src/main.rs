eb5538c3dbba9c8a17ec36df6783148d8dd3b1df
BrownCarter <lilbrown3000@gmail.com>
2022-06-27 23:58:37 +0100

    Solution to length of last word

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-}
 
+}
+#[allow(unused)]
+fn length_of_last_word(s: String) -> i32 {
+    let s1: Vec<&str> = s.trim()
+        .split(" ")
+        .collect();
+    println!("{:?}", s1);
+    let ans: i32 = s1[s1.len() - 1].len() as i32;
+    return ans;
+}
