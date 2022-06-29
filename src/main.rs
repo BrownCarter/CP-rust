10443db0965702981a9df2c35be9f9c6ec66225d
BrownCarter <lilbrown3000@gmail.com>
2022-06-29 03:05:22 +0100

    Solution to move Zero updated

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
-use std::ptr::swap;
-
 fn main() {
-    let mut v1 = vec![0, 1, 0, 3, 12, 0, 1, 8, 7, 0];
+    let mut v1 = vec![1, 0, 0, 0, 0];
     move_zeroes(&mut v1);
     println!("{:?}", v1);
 }
@@ -11,13 +9,10 @@ fn move_zeroes(nums: &mut Vec<i32>) {
         return;
     }
     let mut slow: usize = 0;
-    for fast in 1..nums.len() {
-        if nums[fast] != 0 && nums[slow] == 0 {
-            // TODO learn to do this without using unsafe.
-            unsafe {
-                swap(&mut nums[slow], &mut nums[fast]);
-            }
-            slow += 1;
+    for fast in 0..nums.len() {
+        if nums[fast] != 0 {
+           nums.swap(fast, slow); 
+           slow+=1;
         }
     }
 }
