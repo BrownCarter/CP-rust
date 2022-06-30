149c28d697872b0cc8d60af23bcede289a995260
BrownCarter <lilbrown3000@gmail.com>
2022-06-30 09:47:02 +0100

    Solution to move Zero Corrected

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
 fn main() {
-    let mut v1 = vec![1, 0, 0, 0, 0];
+    let mut v1 = vec![0, 1, 0];
     move_zeroes(&mut v1);
     println!("{:?}", v1);
 }
 
+#[allow(unused)]
 fn move_zeroes(nums: &mut Vec<i32>) {
-    if nums.len() == 0 {
-        return;
-    }
     let mut slow: usize = 0;
     for fast in 0..nums.len() {
         if nums[fast] != 0 {
@@ -16,3 +14,4 @@ fn move_zeroes(nums: &mut Vec<i32>) {
         }
     }
 }
+
