dc13d5b47035d5c5f10bac3614d5bf569fa8dc97
BrownCarter <lilbrown3000@gmail.com>
2022-06-30 10:24:34 +0100

    Solution to first unique character

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::collections::HashMap;
+
 fn main() {
+    let x = String::from("aabb");
+    let y = first_uniq_char(x);
+    println!("{}", y);
 }
 
+fn first_uniq_char(s: String) -> i32 {
+    let mut freq = HashMap::new();
+    for value in s.chars() {
+        *freq.entry(value).or_insert(0) += 1;
+    }
+
+    for (key, value) in s.chars().enumerate() {
+        let check: i32 = *freq.get(&value).unwrap();
+        if check == 1 {
+            return key as i32;
+        }
+    }
+    return -1;
+}
