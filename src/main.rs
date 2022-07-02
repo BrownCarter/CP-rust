d94dfaf9399be30fb60ffd9b5fcf7a0ae1563073
BrownCarter <lilbrown3000@gmail.com>
2022-07-02 05:07:13 +0100

    partial solution

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::collections::HashMap;
+
 fn main() {
+    let v1 = vec![1,1,1,3,3,4,3,2,4,2];
+    let check = contains_duplicate(v1);
+}
+fn contains_duplicate(nums: Vec<i32>) -> bool {
+     let mut freq = HashMap::new();
+     *freq.entry(nums).or_insert(0) + 1;
+     println!("{:#?}", freq);
+     return true;
 }
 
