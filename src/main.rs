310530899939b1980136d4528096aa60c57712b0
BrownCarter <lilbrown3000@gmail.com>
2022-07-02 12:44:00 +0100

    full  solution

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
 use std::collections::HashMap;
 
 fn main() {
-    let v1 = vec![1,1,1,3,3,4,3,2,4,2];
+    let v1 = vec![1, 1];
     let check = contains_duplicate(v1);
+    println!("{}", check);
 }
 fn contains_duplicate(nums: Vec<i32>) -> bool {
-     let mut freq = HashMap::new();
-     *freq.entry(nums).or_insert(0) + 1;
-     println!("{:#?}", freq);
-     return true;
+ let mut freq = HashMap::new();
+ for value in nums.iter(){
+     *freq.entry(value).or_insert(0) += 1;
+ }
+ for (_key, value) in freq.iter() {
+     if value >= &2 {
+         return false;
+     }
+ }
+ return true;
 }
 
