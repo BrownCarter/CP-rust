57996adebd5e5ac9208985603dad88479254b3d0
BrownCarter <lilbrown3000@gmail.com>
2022-07-05 11:46:41 +0100

    Non optmized solution Solution

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
+    let mut v1 = vec![1, 2, 3, 0, 0, 0];
+    let mut v2 = vec![2, 5, 6];
+    merge(&mut v1, 3, &mut v2, 3);
 }
 
+fn merge(nums1: &mut Vec<i32>, m: i32, nums2: &mut Vec<i32>, n: i32) {
+    //TODO optimize from O(nlog(n)) to O(m + n) 
+    for i in 0..n {
+        nums1[(m + i) as usize] = nums2[i as usize];
+    }
+    nums1.sort();
+    println!("{:?}", nums1);
+}
