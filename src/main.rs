bf239782b2fed5e4436fe831ef2ddb5399230c43
BrownCarter <lilbrown3000@gmail.com>
2022-07-07 15:03:00 +0100

    solution to intersect

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,21 @@
 fn main() {
+    let v1 = vec![4, 9, 5];
+    let v2 = vec![9, 4, 9, 8, 4];
+    let v3 = intersect(v1, v2);
+    assert_eq!(v3, vec![9, 4])
 }
 
 fn intersect(nums1: Vec<i32>, nums2: Vec<i32>) -> Vec<i32> {
-                
+    let mut freq = vec![0; 1001];
+    for key in 0..nums1.len() {
+        freq[nums1[key as usize] as usize] += 1;
+    }
+    let mut ans = Vec::new();
+    for i in 0..nums2.len() {
+        if freq[nums2[i] as usize] > 0  {
+            freq[nums2[i] as usize] -=1;
+            ans.push(nums2[i]);
+        }
+    }
+    return ans;
 }
-
