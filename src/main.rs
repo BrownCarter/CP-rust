c07cff1f6f44cda43819cf354e538245f63c600b
BrownCarter <lilbrown3000@gmail.com>
2022-07-07 12:46:08 +0100

    solution to reshape matrix

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
+    let v1 = vec![vec![1, 2], vec![3, 4]];
+    let v2 = matrix_reshape(v1, 2, 4);
+    println!("{:?}", v2);
 }
 
+fn matrix_reshape(mat: Vec<Vec<i32>>, r: i32, c: i32) -> Vec<Vec<i32>> {
+    let mut v1: Vec<Vec<i32>> = vec![vec![0; c as usize]; r as usize];
+    let total = mat[0].len() * mat.len();
+    if r * c != total as i32 {
+        return mat;
+    }
+    let c = c as usize;
+    for row in 0..mat.len() * mat[0].len() {
+        let n = mat[0].len();
+        v1[row/c][row%c] = mat[row / n][row % n];
+    }
+    return v1;
+}
