1ffd47f3e12ed352ac8d87a11f2366cf85eadfaa
BrownCarter <lilbrown3000@gmail.com>
2022-07-08 23:19:56 +0100

    solution to pascal Triangle

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
+    assert_eq!(generate(2), vec![vec![1], vec![1, 1]]);
 }
-
+fn generate(num_rows: i32) -> Vec<Vec<i32>> {
+    let mut v1: Vec<Vec<i32>> = Vec::from(vec![vec![1], vec![1, 1]]);
+    if num_rows == 1 {
+        return  vec![v1[0].clone()];
+    }
+    if num_rows == 2 {
+        return vec![v1[0].clone(), v1[1].clone()];
+    }
+    for i in 3..num_rows as usize + 1{
+        let mut v2 = vec![0; i];
+        let n = &v2.len();
+        v2[0] = 1;
+        v2[*n - 1] = 1;
+        for j in 1..*n - 1 {
+            v2[j] = v1[i - 2][j] + v1[i - 2][j - 1];
+        }
+        v1.push(v2);
+    }
+    return v1;
+}
