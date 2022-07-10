f02ca8b9798c3a7fa10ad5fdb9a37028323bc7b4
BrownCarter <lilbrown3000@gmail.com>
2022-07-10 09:26:38 +0100

    solution to searching 2D matrixe

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
+    let v1 = vec![vec![1, 3, 5, 7], vec![10, 11, 16, 20], vec![23, 30, 34, 60]];
+    assert!(search_matrix(v1, 13));
+    assert!(search_matrix(vec![vec![60]], 60));
 }
 
+fn search_matrix(matrix: Vec<Vec<i32>>, target: i32) -> bool {
+    let m = matrix.len();
+    for row in 0..m {
+        let mut left: i32 = 0;
+        let mut right: i32 = matrix[row].len() as i32 - 1;
+        while left <= right {
+            let mid = (left + right) >> 1;
+            if matrix[row as usize][mid as usize] == target {
+                return true;
+            }
+            if matrix[row as usize][mid as usize] < target {
+                left = mid + 1;
+            }
+            if matrix[row as usize][mid as usize] > target {
+                right = mid - 1;
+            }
+        }
+    }
+    return false;
+}
